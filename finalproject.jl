37a42c4b0f10e3860b17160ea23a98765fe1f69e
tomer arnon <tomerarnon@gmail.com>
2018-12-04 21:37:30 -0800

    sharing new code

--- finalproject.jl
+++ finalproject.jl
@@ -6,11 +6,31 @@ using Statistics
 using LinearAlgebra
 using Random
 using ParticleFilters
+using POMDPSimulators
+using POMDPModelTools
+using BeliefUpdaters
+
+#import just about everything
+import POMDPs: initial_state,
+    discount,
+    n_states,
+    n_actions,
+    states,
+    actions,
+    initialstate_distribution,
+    reward,
+    generate_s,
+    generate_o,
+    generate_sr,
+    observation,
+    transition,
+    stateindex,
+    ordered_states
+
 
 
 """
 TruckState
-
     fault - true/false
     d     - distance since last repair
 """
@@ -19,11 +39,22 @@ struct TruckState
     d::Float64
 end
 
+"""
+TruckAction
+    repair - true/faluse
+    d      - distance of this drive segment
+"""
 struct TruckAction
     repair::Bool
     d::Float64
 end
 
+# """
+# TruckObs
+# """
+# struct TruckObs
+#     o_vec::Vector{Float64}
+# end
 
 """
 TruckMaintenance
@@ -55,7 +86,7 @@ end
 reliability(p::TruckMaintenance, s::TruckState) = exp(-s.d/ p.λ)
 rand_distance(p::TruckMaintenance, rng::AbstractRNG  = Random.GLOBAL_RNG= GLOBAL_RNG) = 3000.0*rand(rng) + 5.0
 
-# getindex(D::Dict, s::TruckState) = D[s.fault]
+Base.getindex(D::Dict, s::TruckState) = D[s.fault]
 
 # random Normal constructor
 function TruckMaintenance(n_sensors::Int; λ = 10_000)
@@ -70,8 +101,8 @@ function TruckMaintenance(n_sensors::Int; λ = 10_000)
         push!(sensor_dict[false], Normal(μ, σ))
 
         ### make slightly different distributions for :fault state
-        # just pick values of 2 for no reason
-        μ += 2*(rand()-0.5)  # ± 1
+        # for now, just pick values of 5, 2 for no reason
+        μ += 5*(rand()-0.5)  # ± 2.5
         σ *= 2*rand()        # scale by 0-2
         push!(sensor_dict[true], Normal(μ, σ))
     end
@@ -82,15 +113,24 @@ end
 initial_state(p::TruckMaintenance) = TruckState(false, 0)
 discount(::TruckMaintenance) = 0.9
 
+n_states(p::TruckMaintenance)  = 2
+n_actions(p::TruckMaintenance) = 2
+ordered_states(p::TruckMaintenance)        = [TruckState(false, 0.0), TruckState(true, 0.0)]
+states(p::TruckMaintenance, s::TruckState) = [TruckState(false, s.d), TruckState(true, s.d)]
+actions(p::TruckMaintenance)               = [TruckAction(false, 1000), TruckAction(true, 1000)]
+stateindex(p::TruckMaintenance, s::TruckState) = 2 - s.fault
+initialstate_distribution(p::TruckMaintenance) = BoolDistribution(0.0)
+
+function POMDPSimulators.get_initialstate(sim::Simulator, initialstate_dist::BoolDistribution)
+    f = rand(sim.rng, initialstate_dist)
+    return TruckState(f, 0.0)
+end
 
 
-
-
-
-function reward(p::TruckMaintenance, s::TruckState, a::TruckAction)
+function reward(p::TruckMaintenance, s::TruckState, a::TruckAction, sp::TruckState)
     r = 0.0
 
-    r -= s.fault  ? 100 : 0
+    r -= sp.fault ? 100 : 0
     r -= a.repair ?  10 : 0
 
     return r
@@ -102,10 +142,11 @@ function generate_s(p::TruckMaintenance, s::TruckState, a::TruckAction, rng::Abs
     end
 
     if s.fault
-        return TruckState(true, s.d+a.d)
+        return TruckState(true, s.d)
     end
     # ∴ s is :nofault —
-    if rand() > reliability(p, s)
+    r = rand()
+    if r > reliability(p, s)
         return TruckState(true, s.d+a.d)
     else
         return TruckState(false, s.d+a.d)
@@ -122,11 +163,6 @@ end
 #     o_vec
 # end
 
-scl(d::Normal, x) = x.*d.σ .+ d.μ
-lerp(a, b, x) = a + x*(b-a)
-
-# TYPE PIRACY
-Distributions.pdf(Nvec::Vector{<:Normal}, x) = prod(pdf.(Nvec, x))
 
 # assumes each sensor measurement is independent and does not depend on previous measurements
 function generate_o(p::TruckMaintenance, s::TruckState, a::TruckAction, sp::TruckState, rng::AbstractRNG = Random.GLOBAL_RNG) # only sp matters
@@ -134,22 +170,24 @@ function generate_o(p::TruckMaintenance, s::TruckState, a::TruckAction, sp::Truc
     nofault_dists = p.sensor_dict[false]
 
     o_vec = zeros(Float64, length(fault_dists))
-    if sp.fault
-        for i in 1:length(o_vec)
-            o_vec[i] = rand(p.sensor_dict[sp][i])
-        end
-        return o_vec
-    end
 
-    for i in 1:length(o_vec)
-        x = randn(rng)
+    o_vec = [rand(rng, x) for x in p.sensor_dict[sp]]
+    # if sp.fault
+        # for i in 1:length(o_vec)
+        #     o_vec[i] = rand(p.sensor_dict[sp][i])
+        # end
+        # return o_vec
+    # end
 
-        fault_obs   = scl(fault_dists[i], x)
-        nofault_obs = scl(nofault_dists[i], x)
+    # for i in 1:length(o_vec)
+    #     x = randn(rng)
 
-        o_vec[i] = lerp(fault_obs, nofault_obs, 0.9*reliability(p, sp)) # when reliability is close to 1, the value is close to nofault but never reaches it
-    end
-    o_vec
+    #     fault_obs   = scl(fault_dists[i], x)
+    #     nofault_obs = scl(nofault_dists[i], x)
+
+    #     o_vec[i] = lerp(fault_obs, nofault_obs, 0.9*reliability(p, sp)) # when reliability is close to 1, the value is close to nofault but never reaches it
+    # end
+    # o_vec
 end
 
 function generate_sr(p::TruckMaintenance, s::TruckState, a::TruckAction, rng::AbstractRNG = Random.GLOBAL_RNG)
@@ -163,7 +201,6 @@ end
 # The other formulation method:
 
 function observation(p::TruckMaintenance, sp::TruckState)
-
     return p.sensor_dict[sp]
 end
 
@@ -173,37 +210,27 @@ function transition(p::TruckMaintenance, s::TruckState, a::TruckAction)
     # elseif:
     s.fault && return BoolDistribution(1.0)
     # else:
-    return BoolDistribution(1-reliability(p, s))
+    rel = reliability(p, s)
+    return BoolDistribution(1-rel)
 end
 
+scl(d::Normal, x) = x.*d.σ .+ d.μ
+lerp(a, b, x) = a + x*(b-a)
+
+# TYPE PIRACY
+Distributions.pdf(Nvec::Vector{<:Normal}, x) = prod(pdf.(Nvec, x))
+POMDPs.pdf(d::BoolDistribution, s::TruckState) = pdf(d, s.fault)
 
 
 
-using MCTS
-using BeliefUpdaters
-using POMDPModelTools
+# using MCTS, BasicPOMCP, QMDP
 
+# p = TruckMaintenance(3)
+# belief_updater = SIRParticleFilter(p, 100)
+# # belief_updater = DiscreteUpdater(p)
 
-p = TruckMaintenance(3)
+# solver = POMCPSolver()
+# solver = BeliefMCTSSolver(DPWSolver(), belief_updater)
+# policy = solve(solver, p)
 
-n_particles = 100
-b = ParticleCollection([initial_state(p) for i in 1:n_particles])
-belief_updater = DiscreteUpdater(p)
-
-s = initial_state(p)
-a = TruckAction(false, 100)
-
-sp = generate_s(p, s, a)
-o = generate_o(p, s, a, sp)
-
-# update(belief_updater, b, a, o)
-
-
-
-
-
-
-
-
-
-
+# history = simulate(HistoryRecorder(max_steps = 20), p, policy, belief_updater)
