3155509ce3a905254eaba7ba1898c8b4ebd3f328
DESKTOP-ME6L2E7\Wouter Van Gijseghem <woutervangijseghem@gmail.com>
2018-12-05 19:39:56 -0800

    for Tomer

--- finalproject.jl
+++ finalproject.jl
@@ -1,4 +1,5 @@
 using POMDPs
+using Plots
 using StaticArrays
 using Distributions
 using StatsBase
@@ -9,6 +10,12 @@ using ParticleFilters
 using POMDPSimulators
 using POMDPModelTools
 using BeliefUpdaters
+using MCTS, BasicPOMCP, QMDP
+using POMDPs
+using POMDPModels # For the problem
+using BasicPOMCP # For the solver
+using POMDPPolicies # For creating a random policy
+using Printf
 
 #import just about everything
 import POMDPs: initial_state,
@@ -89,7 +96,7 @@ rand_distance(p::TruckMaintenance, rng::AbstractRNG = Random.GLOBAL_RNG) = 3000.
 Base.getindex(D::Dict, s::TruckState) = D[s.fault]
 
 # random Normal constructor
-function TruckMaintenance(n_sensors::Int; λ = 10_000)
+function TruckMaintenance(n_sensors::Int; λ = 100_000)
 
     sensor_dict = Dict{Bool, Vector{Normal}}()
     sensor_dict[true]   = Vector{Normal}()
@@ -102,7 +109,7 @@ function TruckMaintenance(n_sensors::Int; λ = 10_000)
 
         ### make slightly different distributions for :fault state
         # for now, just pick values of 5, 2 for no reason
-        μ += 5*(rand()-0.5)  # ± 2.5
+        μ += 5*(rand()-1)  # ± 2.5
         σ *= 2*rand()        # scale by 0-2
         push!(sensor_dict[true], Normal(μ, σ))
     end
@@ -111,7 +118,7 @@ end
 
 
 initial_state(p::TruckMaintenance) = TruckState(false, 0)
-discount(::TruckMaintenance) = 0.9
+discount(::TruckMaintenance) = 0.95
 
 n_states(p::TruckMaintenance)  = 2
 ordered_states(p::TruckMaintenance)                        = states(TruckMaintenance)
@@ -122,7 +129,7 @@ states(p::TruckMaintenance, s::TruckState, a::TruckAction) = [TruckState(false, 
 n_actions(p::TruckMaintenance) = 2
 actions(p::TruckMaintenance) = [TruckAction(false, 1000), TruckAction(true, 1000)]
 stateindex(p::TruckMaintenance, s::TruckState) = 2 - s.fault
-initialstate_distribution(p::TruckMaintenance) = BoolDistribution(0.0)
+initialstate_distribution(p::TruckMaintenance) = SparseCat(states(p), [1.0, 0.0])
 
 function POMDPSimulators.get_initialstate(sim::Simulator, initialstate_dist::BoolDistribution)
     f = rand(sim.rng, initialstate_dist)
@@ -134,7 +141,7 @@ function reward(p::TruckMaintenance, s::TruckState, a::TruckAction, sp::TruckSta
     r = 0.0
 
     r -= sp.fault ? 100 : 0
-    r -= a.repair ?  10 : 0
+    r -= a.repair ?  50 : 0
 
     return r
 end
@@ -168,30 +175,30 @@ end
 
 
 # assumes each sensor measurement is independent and does not depend on previous measurements
-function generate_o(p::TruckMaintenance, s::TruckState, a::TruckAction, sp::TruckState, rng::AbstractRNG = Random.GLOBAL_RNG) # only sp matters
-    fault_dists   = p.sensor_dict[true]
-    nofault_dists = p.sensor_dict[false]
-
-    o_vec = zeros(Float64, length(fault_dists))
-
-    o_vec = [rand(rng, x) for x in p.sensor_dict[sp]]
-    # if sp.fault
-        # for i in 1:length(o_vec)
-        #     o_vec[i] = rand(p.sensor_dict[sp][i])
-        # end
-        # return o_vec
-    # end
-
-    # for i in 1:length(o_vec)
-    #     x = randn(rng)
-
-    #     fault_obs   = scl(fault_dists[i], x)
-    #     nofault_obs = scl(nofault_dists[i], x)
-
-    #     o_vec[i] = lerp(fault_obs, nofault_obs, 0.9*reliability(p, sp)) # when reliability is close to 1, the value is close to nofault but never reaches it
-    # end
-    # o_vec
-end
+# function generate_o(p::TruckMaintenance, s::TruckState, a::TruckAction, sp::TruckState, rng::AbstractRNG = Random.GLOBAL_RNG) # only sp matters
+#     fault_dists   = p.sensor_dict[true]
+#     nofault_dists = p.sensor_dict[false]
+#
+#     o_vec = zeros(Float64, length(fault_dists))
+#
+#     o_vec = [rand(rng, x) for x in p.sensor_dict[sp]]
+#     if sp.fault
+#         for i in 1:length(o_vec)
+#             o_vec[i] = rand(p.sensor_dict[sp][i])
+#         end
+#         return o_vec
+#     end
+#
+#     for i in 1:length(o_vec)
+#         x = randn(rng)
+#
+#         fault_obs   = scl(fault_dists[i], x)
+#         nofault_obs = scl(nofault_dists[i], x)
+#
+#         o_vec[i] = lerp(fault_obs, nofault_obs, 0.9*reliability(p, sp)) # when reliability is close to 1, the value is close to nofault but never reaches it
+#     end
+#     o_vec
+# end
 
 function generate_sr(p::TruckMaintenance, s::TruckState, a::TruckAction, rng::AbstractRNG = Random.GLOBAL_RNG)
     sp = generate_s(p, s, a, rng)
@@ -226,15 +233,62 @@ Distributions.pdf(Nvec::Vector{<:Normal}, x) = prod(pdf.(Nvec, x))
 POMDPs.pdf(d::BoolDistribution, s::TruckState) = pdf(d, s.fault)
 
 
-
-using MCTS, BasicPOMCP, QMDP
-
-p = TruckMaintenance(3)
+reward1 = []
+reward2 = []
+p = TruckMaintenance(10)
 belief_updater = SIRParticleFilter(p, 100)
 # belief_updater = DiscreteUpdater(p)
 
-# solver = POMCPSolver()
-solver = BeliefMCTSSolver(DPWSolver(), belief_updater)
+solver = POMCPSolver()
+# # solver = BeliefMCTSSolver(DPWSolver(), belief_updater)
 policy = solve(solver, p)
+history = simulate(HistoryRecorder(max_steps = 100), p, policy, belief_updater)
+println("Total discounted reward: $(discounted_reward(history))")
+for (s, a, sp) in eachstep(history, "s,a,sp")
+    @printf("s: %-26s  a: %-6s  s': %-26s\n", s, a, sp)
+end
+# s = state_hist(history)[1]
+# a = action(policy, s)
 
-history = simulate(HistoryRecorder(max_steps = 20), p, policy, belief_updater)
+solver = POMCPSolver(tree_queries=5, c=10.0, rng=MersenneTwister(1))
+planner = solve(solver, p)
+a, info = action_info(planner, initialstate_distribution(p), tree_in_info=true)
+
+inchrome(D3Tree(info[:tree], init_expand=5))
+
+
+# for i = 1:50
+#     history = simulate(HistoryRecorder(max_steps = 10), p, policy, belief_updater)
+    # tree = policy.tree
+    # for (s, b, a, o) in eachstep(history, "sbao")
+    #     println("State was $s,")
+    #     println("action $a was taken,")
+    #     println("and observation $o was received.\n")
+    # end
+    # println("MCTS Policy: Discounted reward was $(discounted_reward(history)).")
+
+    # policy = RandomPolicy(p)
+    # history = simulate(HistoryRecorder(max_steps = 10), p, policy, belief_updater)
+
+    # tree = policy.tree
+    # for (s, b, a, o) in eachstep(history, "sbao")
+    #     println("State was $s,")
+    #     println("action $a was taken,")
+    #     println("and observation $o was received.\n")
+    # end
+    # println("Random Policy: Discounted reward was $(discounted_reward(history)).")
+    # push!(reward2,discounted_reward(history))
+# end
+# plot(reward1)
+# n_sensors = 3
+# for i in 1:n_sensors
+#     μ = 10*rand()
+#     σ = 10*rand()
+#     display(display(plot(Normal(μ,σ),lw=3)))
+#
+#     ### make slightly different distributions for :fault state
+#     # for now, just pick values of 5, 2 for no reason
+#     μ += 5*(rand()-1)  # ± 2.5
+#     σ *= 2*rand()        # scale by 0-2
+#     display(display(plot!(Normal(μ ,σ),lw=3)))
+# end
